84341f32d7caaa4535d58542f04d8bdaa81d77d2
TYqinnn <12538125+tyqinnn@user.noreply.gitee.com>
2024-07-25 15:48:39 +0800

    *

--- website/vite.config.ts
+++ website/vite.config.ts
@@ -2,24 +2,20 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
 import basicSsl from '@vitejs/plugin-basic-ssl'
-
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(),basicSsl()],
   server: {
     // 启用 HTTPS
     host: '0.0.0.0',
-    https: {
-
-    },
     port: 8991,
+    https: true,
     // 如果你有自定义的证书和密钥文件，可以这样指定它们的路径
     // https: {
-    //   key: fs.readFileSync('/path/to/server.key'),
-    //   cert: fs.readFileSync('/path/to/server.crt')
-    // }
+    //   key: '/root/license/cer/Nginx/approaching-ai.com.key',
+    //   cert: '/root/license/cer/Nginx/approaching-ai.com.crt'
+    // },
     proxy: {
       '/api': {
         target: 'http://127.0.0.1:8993',
