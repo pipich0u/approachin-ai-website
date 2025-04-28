50413bfc7e6bda6b413541d84820fa065bb1186d
TYqinnn <12538125+tyqinnn@user.noreply.gitee.com>
2025-04-28 20:40:41 +0800

    构建：页面

--- src/page/index/components/develop/index.tsx
+++ src/page/index/components/develop/index.tsx
@@ -2,7 +2,8 @@ import { useEffect, useRef } from 'react';
 import './index.css'
 
 import line from '@/assets/images/dev-line.png'
-import first from '@/assets/images/2023.png'
+import dev_1 from '@/assets/images/2023.png'
+import dev_2 from '@/assets/images/202407.png'
 
 export default function DevelopPage() {
     const scroll1Ref = useRef<HTMLDivElement>(null);
@@ -40,27 +41,29 @@ export default function DevelopPage() {
                 <div className='dev-content scroll1' ref={scroll1Ref}>
                     <div className='dev-ox'>
                         <div className='dev-item top'>
-                            <img src={first} alt="" />
+                            <img src={dev_1} alt="" />
                             <div className='text'>
                                 公司成立<br />
                                 种子轮融资：真知创投
                             </div>
                             <div className='dev-item-title'>2023年底</div>
-                            <img src={line} alt="" />
+                            <img src={line} alt="" className='line_dev' />
                         </div>
                     </div>
                 </div>
 
                 <div className='dev-content scroll2' ref={scroll2Ref}>
                     <div className='dev-ox'>
-                        <div className='dev-item bottom'>
-                            <img src={line} alt="" />
-                            <div className='dev-item-title'>2023年底</div>
+                        <div className='dev-item bottom' style={{ marginLeft: "244px" }}>
+                            <img src={line} alt="" className='line_dev'/>
+                            <div className='dev-item-title'>2024年7月</div>
                             <div className='text'>
-                                公司成立<br />
-                                种子轮融资：真知创投
+                                首创“以存换算”技术<br />
+                                首创“全系统异构协同推理架构”技术<br />
+                                开源“Ktransformers”技术<br />
+                                可支持单卡运行千亿级大模型
                             </div>
-                            <img src={first} alt="" />
+                            <img src={dev_2} alt="" />
                         </div>
                     </div>
                 </div>
