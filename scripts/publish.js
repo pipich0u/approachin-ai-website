8cf2a461081ed5e42a1b7b19dec4900221cb0244
tianyufeng <1710470692@qq.com>
2025-05-08 18:11:51 +0800

    feat: 图片替换

---
+++ scripts/publish.js
@@ -0,0 +1,17 @@
+import execSh from "exec-sh";
+let server = "39.105.158.55";
+
+let remoteDir = `/www/wwwroot/approaching-ai-test`;
+
+// if (process.env.NODE_ENV == "production") {
+//   remoteDir = "/www/wwwroot/zhiwen-mvp";
+// } else if (process.env.NODE_ENV == "development") {
+//   remoteDir = "/www/wwwroot/zhiwen-mvp-dev";
+// } else {
+//   throw new Error("deploy error: empty envName");
+// }
+console.log(remoteDir);
+let shell = `scp -r -P 2024  ./dist/* root@${server}:${remoteDir}`;
+execSh(shell);
+// ssh -p 2024 root@39.105.158.55
+// bebfuq-hoBny0-duqnon
