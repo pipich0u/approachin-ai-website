046fbd9a3750f437952b3ee2b4c346583e7c658c
tyqinnn <t274245534@163.com>
2025-12-05 15:39:58 +0800

    refactor(视频背景): ♻️ 完成

--- src/page/index/components/question/index.tsx
+++ src/page/index/components/question/index.tsx
@@ -1,18 +1,21 @@
 
 import './index.css'
-import up from '../../../../assets/images/up.png'
-import down from '../../../../assets/images/down.png'
-import qxt from '../../../../assets/images/qxt.png'
-import ychs from '../../../../assets/images/ychs.png'
+import audio from '@/assets/audio/audio.mp4'
 
 export default function PageQuestion() {
 
     return (
         <div className='qt-container'>
             <div className='qt-title'>AI落地是否还在为此烦恼？
-                <div>
-                    
-                </div>
+                <video
+                    className="video-bg"
+                    autoPlay
+                    loop
+                    muted
+                    playsInline
+                >
+                    <source src={audio} type="video/mp4" />
+                </video>
             </div>
         </div>
     );
