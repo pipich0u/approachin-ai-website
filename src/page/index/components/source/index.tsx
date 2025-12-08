70a392099f3c1d93f0409ed20f9c517463916404
tyqinnn <t274245534@163.com>
2025-12-08 17:22:09 +0800

    feat(去往模型仓库): ✨ 搭建

--- src/page/index/components/source/index.tsx
+++ src/page/index/components/source/index.tsx
@@ -49,7 +49,22 @@ const SourcePage = () => {
             </div>
         </div>
         <div className='models'>
-
+            <div className='models-container'>
+                <div className='models-center'>
+                    <div className="models-title">
+                        一键解锁 AI 管理与集成新范式！
+                    </div>
+                    <motion.button {...scrollInViewSpringProps} className='models-btn'>
+                        <svg xmlns="http://www.w3.org/2000/svg" className="arr-2" viewBox="0 0 24 24" fill='#fff'>
+                            <path d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z" ></path>
+                        </svg>
+                        <span className="text-white font-[380] bt-text">前往模型仓库</span>
+                        <svg xmlns="http://www.w3.org/2000/svg" className="arr-1" viewBox="0 0 24 24" fill='#fff'>
+                            <path d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z" ></path>
+                        </svg>
+                    </motion.button>
+                </div>
+            </div>
         </div>
     </>
 }
