822cd277804832c3f54390c1207e0db365348cdf
tyqinnn <t274245534@163.com>
2025-12-09 16:22:47 +0800

    fix(开源社区背景图): 🐛 显示问题修复

--- src/page/index/components/source/index.tsx
+++ src/page/index/components/source/index.tsx
@@ -33,7 +33,7 @@ const SourcePage = () => {
                                     <path d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z" ></path>
                                 </svg>
                             </motion.button>
-                            <motion.img {...scrollInViewSpringProps} src={SourceImg1} alt="" className='source_img1' />
+                            <motion.img src={SourceImg1} alt="" className='source_img1' />
                         </div>
                     </div>
                     <div className='source-cont-item source-cont-rig'>
@@ -51,7 +51,7 @@ const SourcePage = () => {
                                     <path d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z" ></path>
                                 </svg>
                             </motion.button>
-                            <motion.img src={SourceImg2} alt="" className='source_img2' />
+                            <motion.img {...scrollInViewSpringProps} src={SourceImg2} alt="" className='source_img2' />
                         </div>
                     </div>
                 </div>
