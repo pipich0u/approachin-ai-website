68fe2f3e6d7a2da7a32614e8f0fe55257de08d0a
tyqinnn <t274245534@163.com>
2025-12-23 10:11:13 +0800

    refactor(KT): ♻️ 分离

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -3,11 +3,11 @@ import Index from "@/page/pc/index/inex"
 import Layout from "@/page/pc/index"
 import ModelsRepository from "@/page/pc/modelsNew/modelsRepostory"
 import CooperatePage from "@/page/pc/cooperate"
-import { KT } from "@/page/pc/KT"
+import { KT } from "@/KT/mob-KT"
 
 // mobile
 import MobileIndex from "@/page/mobile/index/index.tsx"
-import { MobileKT } from "@/page/mobile/KT"
+import { MobileKT } from "@/KT/KT"
 import MobileLayout from "@/page/mobile/index.tsx"
 
 export function createAppRoutes(isMobile: boolean): RouteObject[] {
