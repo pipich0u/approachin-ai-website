778655972fd6468698288a638acd9e9cc798c09d
tyqinnn <t274245534@163.com>
2025-12-29 11:59:20 +0800

    feat(介绍公司首页): ✨ 介绍完成

---
+++ src/page/pc/about/introduction/textConfig.ts
@@ -0,0 +1,22 @@
+
+export const introductionList = {
+    title: '全栈智算引擎·普惠',
+    titleColor: '高效AI',
+    btnText: '联系我们',
+    companyName: '趋境科技',
+    companyTitle: '国际顶尖 AI 推理优化技术团队',
+    companyData: [
+        {
+            num: '30%',
+            title: '博士学位'
+        },
+        {
+            num: '90%',
+            title: '硕士学位'
+        },
+        {
+            num: '清华大学',
+            title: '名校背景'
+        },
+    ]
+}
