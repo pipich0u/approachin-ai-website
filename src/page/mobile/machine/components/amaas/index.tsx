e3d183eda48dbc4c0837604caee06f60a35f0e66
tyqinnn <t274245534@163.com>
2026-01-27 15:15:12 +0800

    fix(一体机移动端): 🐛 修改class

--- src/page/mobile/machine/components/amaas/index.tsx
+++ src/page/mobile/machine/components/amaas/index.tsx
@@ -4,24 +4,24 @@ import { scrollInViewSpringOnceProps } from '@/utils/motionConfig'
 import { machinAmaas } from '@/page/pc/product/machine/textConfig'
 import img from '@/assets/images/img/machine-rig.png'
 export const MobMachineAmaas = () => {
-    return <div className='amaas-container'>
-        <div className='amaas-content'>
-            <motion.div className="amaas-con-title" {...scrollInViewSpringOnceProps}>{machinAmaas.title}</motion.div>
-            <div className='amaas-con-box'>
-                <div className="amaas-con-box-left">
-                    <motion.div className="amaas-con-box-title" {...scrollInViewSpringOnceProps}>{machinAmaas.platform}</motion.div>
-                    <motion.div className="amaas-con-box-list" {...scrollInViewSpringOnceProps}>
+    return <div className='mob-amaas-container'>
+        <div className='mob-amaas-content'>
+            <motion.div className="mob-amaas-con-title" {...scrollInViewSpringOnceProps}>{machinAmaas.title}</motion.div>
+            <div className='mob-amaas-con-box'>
+                <div className="mob-amaas-con-box-left">
+                    <motion.div className="mob-amaas-con-box-title" {...scrollInViewSpringOnceProps}>{machinAmaas.platform}</motion.div>
+                    <motion.div className="mob-amaas-con-box-list" {...scrollInViewSpringOnceProps}>
                         {
                             machinAmaas.list.map((item, index) => {
-                                return <div className='amaas-items'>
+                                return <div className='mob-amaas-items'>
                                     <li key={index}>{item.title}</li>
-                                    <div className='amaas-items-desc' key={index}>{item.desc}</div>
+                                    <div className='mob-amaas-items-desc' key={index}>{item.desc}</div>
                                 </div>
                             })
                         }
                     </motion.div>
                 </div>
-                <motion.div {...scrollInViewSpringOnceProps} className="amaas-con-box-rig">
+                <motion.div {...scrollInViewSpringOnceProps} className="mob-amaas-con-box-rig">
                     <img src={img} alt="" className='selectNone'/>
                 </motion.div>
             </div>
