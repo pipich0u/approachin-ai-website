36cfd4c828f27e4b9bf07ce1723dec4dd3e5ee0c
高欢 <gaohuan@MacBook-Pro-2022.local>
2026-04-07 11:49:20 +0800

    style: 趋境资讯banner与企业介绍统一，300px高度，标题居中

--- src/page/mobile/Information/components/index/index.tsx
+++ src/page/mobile/Information/components/index/index.tsx
@@ -1,17 +1,12 @@
 import './index.css'
 import { motion } from 'motion/react'
 import { initialLoadProps } from '@/utils/motionConfig'
-import { useNavigate } from 'react-router-dom'
-import { trackButtonClick } from '@/utils/umami'
-import { InformationBannerList } from '@/page/pc/Information/textConfig'
 
 export const MobInformationIndex = () => {
-    const navigate = useNavigate();
     return <div className='mob-information-index-container'>
         <div className='mob-in-idx-content'>
-            <motion.div {...initialLoadProps} className="mob-in-idx-content-title">
-                {InformationBannerList.title}
-            </motion.div>
+            <motion.h1 {...initialLoadProps} className="mob-in-idx-content-title">趋境资讯</motion.h1>
+            <motion.span {...initialLoadProps} className="mob-in-idx-content-sub">N e w s</motion.span>
         </div>
     </div>
 }
